a9f293a5ff99af822550d745fa844f5cf14c768f
favio41 <favio41@gmail.com>
2026-08-03 08:12:19 +0100

    Add storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
+/// <reference types="vitest/config" />
 
 // https://vitejs.dev/config/
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import preact from '@preact/preset-vite';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+import { defineConfig } from 'vite';
+
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
 	plugins: [
 		preact({
@@ -11,4 +20,31 @@ export default defineConfig({
 			},
 		}),
 	],
+	test: {
+		projects: [
+			{
+				extends: true,
+				plugins: [
+					// The plugin will run tests for the stories defined in your Storybook config
+					// See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+					storybookTest({
+						configDir: path.join(dirname, '.storybook'),
+					}),
+				],
+				test: {
+					name: 'storybook',
+					browser: {
+						enabled: true,
+						headless: true,
+						provider: playwright({}),
+						instances: [
+							{
+								browser: 'chromium',
+							},
+						],
+					},
+				},
+			},
+		],
+	},
 });
